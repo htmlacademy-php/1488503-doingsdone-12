9b83e0cf6bfdbfc7852f04468bfb1757e201b279
Keks <keks@htmlacademy.ru>
2020-06-15 21:03:00 +0300

    :hatching_chick:

---
+++ script.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var $checkbox = document.getElementsByClassName('show_completed');
+
+if ($checkbox.length) {
+  $checkbox[0].addEventListener('change', function (event) {
+    var is_checked = +event.target.checked;
+
+    var searchParams = new URLSearchParams(window.location.search);
+    searchParams.set('show_completed', is_checked);
+
+    window.location = '/index.php?' + searchParams.toString();
+  });
+}
+
+flatpickr('#date', {
+  enableTime: false,
+  dateFormat: "Y-m-d",
+  locale: "ru"
+});
