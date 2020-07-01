38267db8ac88e0c0e58a368301f84c4c50ff30b3
Obyedkov <60929478+Obyedkov@users.noreply.github.com>
2020-07-01 21:38:56 +0300

    Добавил атрибуты и php с условием  <?php if ($show_complete_tasks == 1) :?> checked <?php endif;?>>

--- index.php
+++ index.php
@@ -1,13 +1,6 @@
 <?php
 // показывать или нет выполненные задачи
 $show_complete_tasks = rand(0, 1) ;
-if ($show_complete_tasks == checked)
-{
-    $show_complete_tasks=true;
-}
-else{
-    $show_complete_tasks=false;
-}
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -79,7 +72,7 @@ else{
 
                     <label class="checkbox">
                         <!--добавить сюда атрибут "checked", если переменная $show_complete_tasks равна единице-->
-                        <input class="checkbox__input visually-hidden show_completed" type="checkbox" checked>
+                        <input class="checkbox__input visually-hidden show_completed" type="checkbox" <?php if ($show_complete_tasks == 1) :?> checked <?php endif;?>>
                         <span class="checkbox__text" >Показывать выполненные</span>
                     </label>
                 </div>
@@ -88,7 +81,7 @@ else{
                     <tr class="tasks__item task">
                         <td class="task__select">
                             <label class="checkbox task__checkbox">
-                                <input class="checkbox__input visually-hidden task__checkbox" type="checkbox" checked>
+                                <input class="checkbox__input visually-hidden task__checkbox" type="checkbox" <?php if ($show_complete_tasks == 1) :?> checked <?php endif;?>>
                                 <span class="checkbox__text">Сделать главную страницу Дела в порядке</span>
                             </label>
                         </td>
