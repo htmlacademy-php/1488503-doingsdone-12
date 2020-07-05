ee89b070655c11871f82b49149783a933de87961
Obyedkov <60929478+Obyedkov@users.noreply.github.com>
2020-07-05 16:32:54 +0300

    Вернул на месте checked

--- index.php
+++ index.php
@@ -72,7 +72,7 @@ $show_complete_tasks=rand(0,1);
 
                     <label class="checkbox">
                         <!--добавить сюда атрибут "checked", если переменная $show_complete_tasks равна единице-->
-                        <input class="checkbox__input visually-hidden show_completed" type="checkbox" checked>
+                        <input class="checkbox__input visually-hidden show_completed" type="checkbox" <?php if ($show_complete_tasks == 1) :?> checked <?php endif;?>>
                         <span class="checkbox__text" >Показывать выполненные</span>
                     </label>
                 </div>
