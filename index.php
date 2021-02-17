a949a98783438bb3698a79ff6cd37b56b4f89f3b
Виктор <60929478+Obyedkov@users.noreply.github.com>
2021-02-17 12:46:10 +0300

    чисто код на php перенес на index.php. Задание я не совсем понял. "Тут  вы вместо массива передаете строку". Верно ли я убрал одинарные кавычки а двойными кавычками оставил?
    Вторая комментария тоже не совсем соображаюсь .
    Сделал так же функция include_templeate поменял название name = main.
    Еще написал вторую функцию название layout.php.

--- index.php
+++ index.php
@@ -1,5 +1,59 @@
 <?php
+
+// показывать или нет выполненные задачи
+$show_complete_tasks = rand(0, 1);
+$categories = ["Входящие", "Учеба", "Работа", "Домашние дела", "Авто",];
+$tasks = [
+    [
+        'task' => 'Собеседование в IT компании',
+        'date_of_completion' => '01.12.2019',
+        'category' => 'Работа',
+        'completed' => false,
+    ],
+    [
+        'task' => 'Выполнить тестовое задание',
+        'date_of_completion' => '25.12.2019',
+        'category' => 'Работа',
+        'completed' => false,
+    ],
+    [
+        'task' => 'Сделать задание первого раздела',
+        'date_of_completion' => '21.12.2019',
+        'category' => 'Учеба',
+        'completed' => true,
+    ],
+    [
+        'task' => 'Встреча с другом',
+        'date_of_completion' => '22.12.2019',
+        'category' => 'Входящие',
+        'completed' => false,
+    ],
+    [
+        'task' => 'Купить корм для кота',
+        'date_of_completion' => 'null',
+        'category' => 'Домашние дела',
+        'completed' => false,
+    ],
+    [
+        'task' => 'Заказать пиццу',
+        'date_of_completion' => 'null',
+        'category' => 'Домашние дела',
+        'completed' => false,
+    ]
+];
+function countTasksForCategory($tasks, $category)
+{
+    $count = 0;
+    foreach ($tasks as $task) {
+        if ($task['category'] === $category) {
+            $count++;
+        }
+    }
+    return $count;
+}
+
 include('templates/layout.php');
-include "templates/main.php";
+include ("templates/main.php");
 //HTML-код главной страницы
-print include_template('layout',"'categories' => $categories, 'tasks' => $tasks,");
+echo include_template('main',"categories => $categories, tasks => $tasks,");
+echo include_template('layout.php', "title => $title, content => $content");
--- templates/layout.php
+++ templates/layout.php
@@ -1,60 +1,8 @@
-<?php
-// показывать или нет выполненные задачи
-$show_complete_tasks=rand(0,1);
-$categories = ["Входящие", "Учеба", "Работа", "Домашние дела", "Авто",];
-$tasks = [
-    [
-        'task' => 'Собеседование в IT компании',
-        'date_of_completion' => '01.12.2019',
-        'category' => 'Работа',
-        'completed' => false,
-    ],
-    [
-        'task' => 'Выполнить тестовое задание',
-        'date_of_completion' => '25.12.2019',
-        'category' => 'Работа',
-        'completed' => false,
-    ],
-    [
-        'task' => 'Сделать задание первого раздела',
-        'date_of_completion' => '21.12.2019',
-        'category' => 'Учеба',
-        'completed' => true,
-    ],
-    [
-        'task' => 'Встреча с другом',
-        'date_of_completion' => '22.12.2019',
-        'category' => 'Входящие',
-        'completed' => false,
-    ],
-    [
-        'task' => 'Купить корм для кота',
-        'date_of_completion' => 'null',
-        'category' => 'Домашние дела',
-        'completed' => false,
-    ],
-    [
-        'task' => 'Заказать пиццу',
-        'date_of_completion' => 'null',
-        'category' => 'Домашние дела',
-        'completed' => false,
-    ]
-];
-function countTasksForCategory($tasks, $category){
-    $count = 0;
-    foreach ($tasks as $task){
-        if ($task['category'] === $category){
-            $count++;
-        }
-    }
-    return $count;
-}
-?>
 <!DOCTYPE html>
 <html lang="ru">
 <head>
     <meta charset="UTF-8">
-    <title><?= $title; ?></title>
+    <title><?=$title; ?></title>
     <link rel="stylesheet" href="css/normalize.css">
     <link rel="stylesheet" href="css/style.css">
     <link rel="stylesheet" href="css/flatpickr.min.css">
@@ -77,7 +25,7 @@ function countTasksForCategory($tasks, $category){
                 </div>
             </div>
         </header>
-        <div class="content"><?print ($content); ?></div>
+        <div class="content"><?=$content; ?></div>
     </div>
 </div>
 <footer class="main-footer">
