01b2ff7ffc46809b5a95f684c0a27cf748c222f8
Виктор <vratar1001@gmail.com>
2021-06-22 17:35:33 +0300

    'completed' => $row['status'] == 1 не работает

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
-include('helpers.php');
+include 'helpers.php';
+include 'conndb.php';
+
 // показывать или нет выполненные задачи
 $show_complete_tasks = rand(0, 1);
 $categories = ["Входящие", "Учеба", "Работа", "Домашние дела", "Авто",];
@@ -51,9 +53,36 @@ function countTasksForCategory($tasks, $category)
     }
     return $count;
 }
-$mainContent = include_template('main.php',['categories' => $categories, 'tasks'=> $tasks, 'show_complete_tasks' => $show_complete_tasks]);
+
+//Подключение базы данных
+$conn = new mysqli($servername, $username, $password, $database);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sqlProject = "SELECT * FROM projects";
+$result = mysqli_query($conn, $sqlProject);
+$rows = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+foreach ($rows as $row) {
+    $categories[''] = $row['name'];
+}
+
+$sqlUsers = "SELECT * FROM tasks";
+$result2 = mysqli_query($conn, $sqlUsers);
+$rows2 = mysqli_fetch_all($result2, MYSQLI_ASSOC);
+
+foreach ($rows2 as $row) {
+    if (!empty($row['name']) && !empty($row['project_id']) ) {
+        $tasks[] = [
+            'task' => $row['name'],
+            'date_of_completion' => $row['date_term'],
+            'category' => $row['project_id'],
+            'completed' => $row['status'] == 1,
+        ];
+    }
+}
+
+$mainContent = include_template('main.php', ['categories' => $categories, 'tasks' => $tasks, 'show_complete_tasks' => $show_complete_tasks]);
 echo include_template('layout.php', ['title' => 'Дела в порядке', 'content' => $mainContent]);
-
 //HTML-код главной страницы
-
-
