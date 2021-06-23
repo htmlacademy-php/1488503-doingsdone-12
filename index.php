3c1cba89123680ee4cfbe44da4378ebf3276df39
Виктор <vratar1001@gmail.com>
2021-06-23 11:05:22 +0300

    module5-task1 вторая проверка

--- index.php
+++ index.php
@@ -4,45 +4,78 @@ include 'conndb.php';
 
 // показывать или нет выполненные задачи
 $show_complete_tasks = rand(0, 1);
-$categories = ["Входящие", "Учеба", "Работа", "Домашние дела", "Авто",];
-$tasks = [
-    [
-        'task' => 'Собеседование в IT компании',
-        'date_of_completion' => '10.05.2021',
-        'category' => 'Работа',
-        'completed' => false,
-    ],
-    [
-        'task' => 'Выполнить тестовое задание',
-        'date_of_completion' => '09.05.2021',
-        'category' => 'Работа',
-        'completed' => false,
-    ],
-    [
-        'task' => 'Сделать задание первого раздела',
-        'date_of_completion' => '07.05.2021',
-        'category' => 'Учеба',
-        'completed' => false,
-    ],
-    [
-        'task' => 'Встреча с другом',
-        'date_of_completion' => '06.05.2021',
-        'category' => 'Входящие',
-        'completed' => false,
-    ],
-    [
-        'task' => 'Купить корм для кота',
-        'date_of_completion' => '05.05.2021',
-        'category' => 'Домашние дела',
-        'completed' => false,
-    ],
-    [
-        'task' => 'Заказать пиццу',
-        'date_of_completion' => '08.05.2021',
-        'category' => 'Домашние дела',
-        'completed' => false,
-    ]
-];
+//$categories = ["Входящие", "Учеба", "Работа", "Домашние дела", "Авто",];
+//$tasks = [
+//    [
+//        'task' => 'Собеседование в IT компании',
+//        'date_of_completion' => '10.05.2021',
+//        'category' => 'Работа',
+//        'completed' => false,
+//    ],
+//    [
+//        'task' => 'Выполнить тестовое задание',
+//        'date_of_completion' => '09.05.2021',
+//        'category' => 'Работа',
+//        'completed' => false,
+//    ],
+//    [
+//        'task' => 'Сделать задание первого раздела',
+//        'date_of_completion' => '07.05.2021',
+//        'category' => 'Учеба',
+//        'completed' => false,
+//    ],
+//    [
+//        'task' => 'Встреча с другом',
+//        'date_of_completion' => '06.05.2021',
+//        'category' => 'Входящие',
+//        'completed' => false,
+//    ],
+//    [
+//        'task' => 'Купить корм для кота',
+//        'date_of_completion' => '05.05.2021',
+//        'category' => 'Домашние дела',
+//        'completed' => false,
+//    ],
+//    [
+//        'task' => 'Заказать пиццу',
+//        'date_of_completion' => '08.05.2021',
+//        'category' => 'Домашние дела',
+//        'completed' => false,
+//    ]
+//];
+$categories = [];
+$tasks = [];
+
+//Подключение базы данных
+$conn = new mysqli($servername, $username, $password, $database);
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+$conn->set_charset("utf8");
+
+$sqlProject = "SELECT * FROM projects";
+$result = mysqli_query($conn, $sqlProject);
+$rows = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+foreach ($rows as $row) {
+    $categories[] = $row['name'];
+}
+
+$sqlUsers = "SELECT * FROM tasks";
+$result2 = mysqli_query($conn, $sqlUsers);
+$rows2 = mysqli_fetch_all($result2, MYSQLI_ASSOC);
+
+foreach ($rows2 as $row) {
+    if (!empty($row['name']) && !empty($row['project_id'])) {
+        $tasks[] = [
+            'task' => $row['name'],
+            'date_of_completion' => $row['date_term'],
+            'category' => $row['project_id'],
+            'completed' => $row['status'] == 1,
+        ];
+    }
+}
+
 function countTasksForCategory($tasks, $category)
 {
     $count = 0;
@@ -52,35 +85,6 @@ function countTasksForCategory($tasks, $category)
         }
     }
     return $count;
-}
-
-//Подключение базы данных
-$conn = new mysqli($servername, $username, $password, $database);
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-$sqlProject = "SELECT * FROM projects";
-$result = mysqli_query($conn, $sqlProject);
-$rows = mysqli_fetch_all($result, MYSQLI_ASSOC);
-
-foreach ($rows as $row) {
-    $categories[''] = $row['name'];
-}
-
-$sqlUsers = "SELECT * FROM tasks";
-$result2 = mysqli_query($conn, $sqlUsers);
-$rows2 = mysqli_fetch_all($result2, MYSQLI_ASSOC);
-
-foreach ($rows2 as $row) {
-    if (!empty($row['name']) && !empty($row['project_id']) ) {
-        $tasks[] = [
-            'task' => $row['name'],
-            'date_of_completion' => $row['date_term'],
-            'category' => $row['project_id'],
-            'completed' => $row['status'] == 1,
-        ];
-    }
 }
 
 $mainContent = include_template('main.php', ['categories' => $categories, 'tasks' => $tasks, 'show_complete_tasks' => $show_complete_tasks]);
