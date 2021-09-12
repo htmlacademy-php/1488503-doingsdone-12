4799def0944bd43a9d27d2324bef7e235a437e8a
Obyedkov <vratar1001@gmail.com>
2021-09-13 00:36:59 +0300

    module7-task2 layout.php исправил =

--- templates/layout.php
+++ templates/layout.php
@@ -20,7 +20,7 @@
                 <a class="main-header__side-item button button--plus open-modal" href="add.php">Добавить задачу</a>
                 <div class="main-header__side-item user-menu">
                     <div class="user-menu__data">
-                        <p><?php strip_tags($_SESSION['user']['name']) ?></p>
+                        <p><?= strip_tags($_SESSION['user']['name']) ?></p>
                         <a href="logout.php">Выйти</a>
 
                     </div>
