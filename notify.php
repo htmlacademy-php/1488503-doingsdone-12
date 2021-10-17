9586286ca4f864c27522b2b97ed00a279ef387bb
Obyedkov <vratar1001@gmail.com>
2021-10-17 12:51:55 +0300

    module9-task1 SwiftMailer 2

--- notify.php
+++ notify.php
@@ -3,59 +3,53 @@ require('vendor/autoload.php');
 include 'conndb.php';
 require_once 'connSwiftMailer.php';
 $conn = new mysqli($servername, $username, $password, $database);
+$transport = (new Swift_SmtpTransport('smtp.mail.ru', 465, 'ssl'))
+    ->setUsername($emailSwiftMailer)
+    ->setPassword($passwordSwiftMailer);
+$mailer = new Swift_Mailer($transport);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
-$conn->set_charset("utf8");
 //1. Выполнить SQL запрос на получение всех невыполненных задач (статус равен нулю), у которых срок равен текущему дню.
 $id = [];
+$array = [];
 $date = new DateTime();
 $dateToday = $date->format('Y-m-d');
 $date_start = $dateToday . ' 00:00:00';
 $date_end = $dateToday . ' 23:59:00';
 $where = "status is null or status !=1 AND date_term >= '$date_start' AND date_term <= '$date_end' ";
 $noTasks = mysqli_query($conn,
-    "SELECT tasks.user_id, tasks.name, tasks.date_term, users.name, users.email
+    "SELECT tasks.user_id, tasks.name, tasks.date_term, users.name AS user_name, users.email
                FROM tasks INNER JOIN users ON tasks.user_id = users.id WHERE $where ");
+
 $resSql = mysqli_fetch_all($noTasks, MYSQLI_ASSOC);
 
-foreach ($resSql as $item) {
-    if (!empty($item['user_id'])) {
-        $array[$item['user_id']][] = [
-            'name' => $item['name'],
-            'date_term' => $item['date_term'],
-            'email' => $item['email'],
-        ];
+if (isset($resSql)) {
+    foreach ($resSql as $item) {
+        if (!empty($item['user_id'])) {
+            $array[$item['user_id']][] = [
+                'name' => $item['name'],
+                'date_term' => $item['date_term'],
+                'email' => $item['email'],
+                'user_name' => $item['user_name'],
+            ];
+        }
     }
-
-    $message = (new Swift_Message('Уведомление от сервиса «Дела в порядке»'))
-
-        ->setFrom(['vratar89@bk.ru' => 'Viktor'])
-
-        ->setTo([$item['email'] => $item['name']])
-
-        ->setBody('Уважаемый, ' . $item['name']. '. У вас запланирована задача '.$item['date_term'].' на %время задачи%')
-    ;
-
-    $result = $mailer->send($message);
+    foreach ($array as $userId => $tasks) {
+        $userName = 'Здравствуйте ' . $tasks[0]['user_name'] . "\n";
+        $bodyText = $userName;
+        foreach ($tasks as $task) {
+            $bodyText .= 'У вас запланирована задача ' . $task['name'] . "\n" . ' на ' . $task['date_term'] . '';
+        }
+        $message = (new Swift_Message('Уведомление от сервиса «Дела в порядке»'))
+            ->setFrom(['vratar89@bk.ru' => 'Viktor'])
+            ->setTo(['vratar1001@gmail.com' => 'Артем'])
+            ->setBody($bodyText);
+        $result = $mailer->send($message);
+        if (!extension_loaded('openssl')) {
+            echo "No openssl";
+        }
+    }
 }
-// здесь указываем адрес администратора, который получит заявку с сайта
-// если получателей несколько, указываем в формате: ['receiver@domain.org', 'other@domain.org' => 'A NAME']
-// (там где 'A NAME' пишем любое имя, это ни на что не влияет)
-//$to = ['email_адрес_администратора' => 'ADMIN'];
-//$from = 'ваш_логин@домен_почтового_сервера_например_yandex.ru';
-//Отправитель from
-//$from = '';
-//Получатель to
-//$to = '';
-//if (!extension_loaded('openssl')) {
-//    echo "no openssl extension loaded.";
-//}
-//$emailLogin = "vratar1001@gmail.com";
-//$emailPassword = "Smile4Me1)))";
-//
-//$transport = (new Swift_SmtpTransport('smtp.gmail.com', 465))
-//    ->setUsername($emailLogin)
-//    ->setPassword($emailPassword);
-////var_dump($transport);
-//$mailer = new Swift_Mailer($transport);
+
+
